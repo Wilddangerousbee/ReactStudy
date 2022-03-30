a670467c1f1289a84872dd516b13f60db01a171d
User <User@5CD0535MVN.phoenixit.ru>
2022-03-30 14:37:15 +0300

    study JSX

--- app-study/src/index.js
+++ app-study/src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 
-const element = (
-  <div>
-    <h1>My Todo List</h1>
+const TodoList = () => {
+  const items = ['Learn React', 'Build New Project'];
+  return (
     <ul>
-      <li>Lern React</li>
-      <li>Build Awesome App</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
-  </div>
-);
+  )
+}
 
-reactDOM.render(element, 
+const ArticlesList = () => {
+  return (
+    <h1>My Todo List</h1>
+  )
+}
+
+const SearchPanel = () => {
+  const searchStyle = {
+    frontsize: '20px'
+  };
+
+  return (
+    <input 
+      style={searchStyle} 
+      placeholder='Search'
+      disabled/>
+  )
+}
+
+const App = () => {
+  
+  return (
+    <div>
+      <ArticlesList />
+      <SearchPanel/>
+      <TodoList />
+    </div>
+  )
+}
+
+
+reactDOM.render(<App />, 
   document.getElementById("root"));
