4ffe041899b610f9add8215d1a8ecf740746784b
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-01 17:39:11 +0300

    delete trashing features

--- app-study/src/components/search-panel/search-panel.js
+++ app-study/src/components/search-panel/search-panel.js
@@ -3,16 +3,10 @@ import React from "react";
 import "./search-panel.css"
 
 const SearchPanel = () => {
-    const searchStyle = {
-      frontsize: '20px'
-    };
   
     return (
       <input className="form-control search-input"
-        type="text"
-        style={searchStyle} 
-        placeholder='Search'
-        disabled/>
+        placeholder='Search'/>
     )
 };
 
